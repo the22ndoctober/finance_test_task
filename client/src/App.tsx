3ae2264ea01038773dcfe0d4f6f172715dc7b2b5
the22ndoctober <the22thoctober@gmail.com>
2023-07-20 15:54:10 +0300

    prettier setup

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Container from "./components/container/Container";
+
+function App() {
+  const a = 11;
+
+  return (
+    <div className="App">
+      <Container />
+      sosi yaica
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/container/Container.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Container = () => {
+  let a = 123
+
+  return (
+    <div>
+      <div>Container</div>
+    </div>
+  )
+}
+
+export default Container
